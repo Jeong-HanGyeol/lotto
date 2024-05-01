f011d7a62b262924aafd89fbfe49d1b081ff3df3
Jeong-HanGyeol <gksruf05121@gmail.com>
2024-05-01 17:49:13 +0900

    홈 - 완료

--- app/automatic-lotto/NumCreate.tsx
+++ app/automatic-lotto/NumCreate.tsx
@@ -1,4 +1,40 @@
+"use client";
+
+import { useState } from "react";
+
 const NumCreate = () => {
+  const [randomNumbers, setRandomNumbers] = useState<number[]>([]);
+  const [copiedNumbers, setCopiedNumbers] = useState("");
+
+  const getRandomNumbers = (
+    min: number,
+    max: number,
+    count: number
+  ): number[] => {
+    const numbers = new Set<number>();
+    while (numbers.size < count) {
+      const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+      numbers.add(randomNumber);
+    }
+    return Array.from(numbers).sort((a, b) => a - b);
+  };
+
+  const generateRandomNumbers = () => {
+    const numbers: number[] = getRandomNumbers(1, 45, 6);
+    setRandomNumbers(numbers);
+
+    setCopiedNumbers(numbers.join(", "));
+  };
+
+  const copyToClipboard = async () => {
+    try {
+      await navigator.clipboard.writeText(copiedNumbers);
+      alert(copiedNumbers);
+    } catch (error) {
+      console.error("복사 실패:", error);
+    }
+  };
+
   return (
     <div className="bg-white rounded-full h-16 shadow-md flex items-center justify-between px-7">
       <div className="flex items-center">
@@ -7,59 +43,54 @@ const NumCreate = () => {
         </div>
         <div className="flex items-center justify-between px-14 gap-10">
           <div className="flex gap-4 items-center">
-            <span
-              className={
-                "flex justify-center items-center text-white w-8 h-8 rounded-full bg-red-500"
-              }
-            >
-              7
-            </span>
-            <span
-              className={
-                "flex justify-center items-center text-white w-8 h-8 rounded-full bg-red-500"
-              }
-            >
-              7
-            </span>
-            <span
-              className={
-                "flex justify-center items-center text-white w-8 h-8 rounded-full bg-red-500"
-              }
-            >
-              7
-            </span>
-            <span
-              className={
-                "flex justify-center items-center text-white w-8 h-8 rounded-full bg-red-500"
-              }
-            >
-              7
-            </span>
-            <span
-              className={
-                "flex justify-center items-center text-white w-8 h-8 rounded-full bg-red-500"
-              }
-            >
-              7
-            </span>
-            <span
-              className={
-                "flex justify-center items-center text-white w-8 h-8 rounded-full bg-red-500"
-              }
-            >
-              7
-            </span>
+            {randomNumbers.map((num, i) => (
+              <Ball num={num} key={String(num) + i} />
+            ))}
           </div>
-          <button type="button" className="text-sm text-gray-400">
-            복사
-          </button>
+          {randomNumbers.length > 0 ? (
+            <button
+              type="button"
+              className="text-sm text-gray-400"
+              onClick={copyToClipboard}
+            >
+              복사
+            </button>
+          ) : null}
         </div>
       </div>
 
-      <button type="button" className="text-xl font-extrabold text-blue-500">
+      <button
+        type="button"
+        className="text-xl font-extrabold text-blue-500"
+        onClick={generateRandomNumbers}
+      >
         번호 생성
       </button>
     </div>
+  );
+};
+
+const Ball = ({ num }: { num: number }) => {
+  const getColor = () => {
+    if (num >= 1 && num <= 10) {
+      return "bg-yellow-500";
+    } else if (num >= 11 && num <= 20) {
+      return "bg-blue-500";
+    } else if (num >= 21 && num <= 30) {
+      return "bg-red-500";
+    } else if (num >= 31 && num <= 40) {
+      return "bg-gray-500";
+    } else {
+      return "bg-green-500";
+    }
+  };
+
+  return (
+    <span
+      className={`${getColor()} flex justify-center items-center text-white w-8 h-8 rounded-full`}
+    >
+      {num}
+    </span>
   );
 };
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,55 @@
-export default function Home() {
-  return <div>로또</div>;
+import NumCreate from "./automatic-lotto/NumCreate";
+
+export default function AutomaticLotto() {
+  return (
+    <div className="w-[1080px] mx-auto ">
+      <div className="mt-14 mb-12">
+        <img
+          src="/lottoAi.png"
+          alt="로또 그림"
+          className=" max-h-[224px] mx-auto"
+        />
+      </div>
+
+      <div className="flex flex-col w-[800px] mx-auto gap-6">
+        <NumCreate />
+
+        <PremiumBanner />
+      </div>
+
+      <Ad />
+    </div>
+  );
 }
+
+const PremiumBanner = () => {
+  return (
+    <div className="mx-auto w-[511px] h-[60px] px-10 bg-gray-900 rounded-full">
+      <iframe
+        src="https://ads-partners.coupang.com/widgets.html?id=776177&template=carousel&trackingCode=AF1316385&subId=&width=431&height=60&tsource="
+        width="431"
+        height="60"
+        // frameborder="0"
+        // scrolling="no"
+        // referrerpolicy="unsafe-url"
+        // browsingtopics
+      ></iframe>
+    </div>
+  );
+};
+
+const Ad = () => {
+  return (
+    <div className="w-full min-h-[250px] mt-6 flex max-w-[1080px] mx-auto bg-white py-2 rounded-md items-center justify-center">
+      <iframe
+        src="https://ads-partners.coupang.com/widgets.html?id=776178&template=carousel&trackingCode=AF1316385&subId=&width=728&height=200&tsource="
+        width="728"
+        height="200"
+        // frameborder="0"
+        // scrolling="no"
+        // referrerpolicy="unsafe-url"
+        // browsingtopics
+      ></iframe>
+    </div>
+  );
+};
