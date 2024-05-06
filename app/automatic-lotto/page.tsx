f3419fbda765399c1b530e714a8f0fdaa14ceaa0
Jeong-HanGyeol <gksruf05121@gmail.com>
2024-05-06 15:50:35 +0900

    로또 자동 추출기 - BannerContainer 높이 수정

--- app/automatic-lotto/page.tsx
+++ app/automatic-lotto/page.tsx
@@ -2,16 +2,16 @@ import ContentHeader from "./ContentHeader";
 
 const AutomaticLotto = () => {
   return (
-    <>
+    <div className="bg-gray-200">
       <ContentHeader />
       <BannerContainer />
-    </>
+    </div>
   );
 };
 
 const BannerContainer = () => {
   return (
-    <div className="w-full min-h-24 mt-2 flex justify-center max-w-[1080px] mx-auto bg-white rounded py-2"></div>
+    <div className="w-full min-h-[106px] mt-2 flex justify-center max-w-[1080px] mx-auto bg-white rounded py-2"></div>
   );
 };
 
