939294a108a434c3987050e16ec9b02ab7dc549d
Jeong-HanGyeol <gksruf05121@gmail.com>
2024-05-06 18:36:24 +0900

    footer - pathname에 따른 컬러변화

--- app/common/Footer/FooterTop.tsx
+++ app/common/Footer/FooterTop.tsx
@@ -1,14 +1,29 @@
-const FooterTop = () => {
+const FooterTop = ({ path }: { path: boolean }) => {
   return (
     <section className="flex justify-between">
-      <strong className="font-extrabold text-white pb-4 text-2xl">
+      <strong
+        className={`${
+          path ? "text-white" : "text-gray-500"
+        } font-extrabold pb-4 text-2xl`}
+      >
         <a href="/">JEONG G</a>
       </strong>
 
       <div className="ml-3">
-        <strong className="font-bold text-white pb-4 block">Products</strong>
+        <strong
+          className={`${
+            path ? "text-white" : "text-gray-900"
+          } font-medium pb-4 block text-sm`}
+        >
+          Products
+        </strong>
         <nav className="flex gap-1 flex-col">
-          <a href="/" className="text-white text-sm inline-block">
+          <a
+            href="/"
+            className={`${
+              path ? "text-white" : "text-gray-500"
+            } text-sm inline-block`}
+          >
             로또
           </a>
         </nav>
--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,23 @@
-import { NextRequest, NextResponse, userAgent } from "next/server";
+// import { NextRequest, NextResponse, userAgent } from "next/server";
+
+// export function middleware(request: NextRequest) {
+//   const { device } = userAgent(request);
+//   const viewport = device.type === "mobile" ? "mobile" : "desktop";
+
+//   request.nextUrl.searchParams.set("viewport", viewport);
+
+//   return NextResponse.rewrite(request.nextUrl);
+// }
+
+import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const { device } = userAgent(request);
-  const viewport = device.type === "mobile" ? "mobile" : "desktop";
+  const requestHeaders = new Headers(request.headers);
+  requestHeaders.set("x-pathname", request.nextUrl.pathname);
 
-  request.nextUrl.searchParams.set("viewport", viewport);
-
-  return NextResponse.rewrite(request.nextUrl);
+  return NextResponse.next({
+    request: {
+      headers: requestHeaders,
+    },
+  });
 }
