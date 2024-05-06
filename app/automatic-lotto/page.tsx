4049045c59febbc820f18c875bb2488f0705dd46
Jeong-HanGyeol <gksruf05121@gmail.com>
2024-05-06 15:47:47 +0900

    로또 자동 추출기 - BannerContainer add

--- app/automatic-lotto/page.tsx
+++ app/automatic-lotto/page.tsx
@@ -4,7 +4,14 @@ const AutomaticLotto = () => {
   return (
     <>
       <ContentHeader />
+      <BannerContainer />
     </>
+  );
+};
+
+const BannerContainer = () => {
+  return (
+    <div className="w-full min-h-24 mt-2 flex justify-center max-w-[1080px] mx-auto bg-white rounded py-2"></div>
   );
 };
 
